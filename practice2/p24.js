b2227af74c61dc8db23b2ed3e6408024bfbc9cfe
이예린 <h2002y@naver.com>
2024-08-02 16:29:38 +0900

    Add: 2강 24번째 강의 실습 추가 - 배열 at 메서드

---
+++ practice2/p24.js
@@ -0,0 +1,7 @@
+// 배열 at 메서드
+// 배열의 특정 위치를 가져옴
+const fruits = ['apple', 'banana', 'cherry', 'banana', 'mango', 'banana'];
+console.log(fruits.at(0)); // apple
+
+// at 메서드에는 -를 넣을 수 있다
+console.log(fruits.at(-1)); // banana
